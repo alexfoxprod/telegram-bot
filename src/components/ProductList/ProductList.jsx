8245d889a4f189f9c8b7bfc79b01cbb3f7b43faa
alexfoxprod <oleksandr.lutsiuk@gmail.com>
2024-05-23 20:37:27 +0300

    change products

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,14 +4,14 @@ import ProductItem from '../ProductItem/ProductItem'
 import { useTelegram } from '../../hooks/useTelegram'
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинси', price: 5000, description: 'Cинього кольору, прямі'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Чорного кольору, бомбер'},
+    {id: '3', title: 'Джинси 2', price: 5000, description: 'Cинього кольору, прямі'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Чорного кольору, бомбер'},
+    {id: '5', title: 'Джинси 3', price: 5000, description: 'Cинього кольору, прямі'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Чорного кольору, бомбер'},
+    {id: '7', title: 'Джинси 4', price: 5500, description: 'Cинього кольору, прямі'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Чорного кольору, бомбер'},
 ]
 
 const getTotalPrice = (items = []) => {
