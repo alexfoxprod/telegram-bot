60259fb1115ae91891ed0a6e308d3dcf27374d51
alexfoxprod <oleksandr.lutsiuk@gmail.com>
2024-05-23 14:08:17 +0300

    fix-routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import { useEffect } from 'react';
-import { useTelegram } from './hooks/useTelegram'; 
-import Header from './components/Header/Header';
-import { Route, Routes  } from 'react-router';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+import "./App.css";
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  const {tg} = useTelegram();
+  const { tg } = useTelegram();
 
-  useEffect( () => {
+  useEffect(() => {
     tg.ready();
-  }, [tg])
+  }, [tg]);
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-        <Route index element={<ProductList/>}/>
-        <Route path={"form"} element={<Form/>}/>
+        <Route index element={<ProductList />} />
+        <Route path={"form"} element={<Form />} />
       </Routes>
     </div>
   );
