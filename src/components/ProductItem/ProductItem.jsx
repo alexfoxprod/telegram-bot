016e2119c7f1e71fa11e9d95c69def76c3b5d660
alexfoxprod <oleksandr.lutsiuk@gmail.com>
2024-05-23 20:29:23 +0300

    add-products

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./ProductItem.css"
+import Button from '../Button/Button'
+
+const ProductItem = ({product, className}) => {
+
+    const onAddHandler = () => {
+        onAdd(product)
+    }
+  return (
+    <div className={'product ' + className}>
+        <di className={'img'}/>
+        <div className={'title'}>{product.title}</div>
+        <div className={'description'}>{product.description}</div>
+        <div className={'price'}>
+            <span>Вартіст: <b>{product.price}</b></span>
+        </div>
+        <Button className={'add-btn'} onClick={onAddHandler}> Додати в кошик</Button>
+    </div>
+  )
+}
+
+export default ProductItem
