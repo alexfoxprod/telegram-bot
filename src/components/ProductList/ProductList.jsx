a9c2122dac21dc5c51d6d8dcfb907417fc74f606
alexfoxprod <oleksandr.lutsiuk@gmail.com>
2024-05-29 14:18:00 +0300

    create-product-bucket

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -20,7 +20,30 @@ const getTotalPrice = (items = []) => {
 const ProductList = () => {
 
     const [addedItems, setAddedItems] = useState([])
-    const {tg} = useTelegram()
+    const {tg, queryId} = useTelegram()
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId
+        }
+        fetch("http://localhost:8000/api/telegram", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data) 
+        })
+        tg.sendData(JSON.stringify(data))
+    }, [country, street, subject])
+ 
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData]) 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id)
         let newItems = [];
